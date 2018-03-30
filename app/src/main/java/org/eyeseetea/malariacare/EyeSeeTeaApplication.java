ca96b4e286e570349963de9201821a87c60e6b0c
ifoche <nacho.foche@gmail.com>
2018-03-30 19:39:23 +0200

    point bugs to EReferralsApp repository

--- app/src/main/java/org/eyeseetea/malariacare/EyeSeeTeaApplication.java
+++ app/src/main/java/org/eyeseetea/malariacare/EyeSeeTeaApplication.java
@@ -130,7 +130,7 @@ public class EyeSeeTeaApplication extends Application {
                 .setLoggingEnabled(BuildConfig.DEBUG)
                 .setAlertDialogType(AlertDialogType.APP_COMPAT)
                 .setGitHubInfo(new GitHubConfiguration(
-                        "eyeseetea/pictureapp",
+                        "eyeseetea/EReferralsApp",
                         BuildConfig.GIT_HUB_BOT_TOKEN,
                         "eyeseeteabottest/snapshots",
                         "master"))
